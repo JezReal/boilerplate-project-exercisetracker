663b9690943e081c29d746b26864377818a2e99e
Jezreel <jezreeljoshuam@gmail.com>
2023-05-07 23:43:44 +0800

    implement

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { v4: uuidv4 } = require('uuid');
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+users = [];
+exercises = [];
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.post("/api/users", (request, response) => {
+  const uuid = uuidv4();
 
+  users.push({username: request.body.username, _id: uuid});
 
+  response.json({
+    username: request.body.username,
+    _id: uuid,
+  });
+});
 
+app.get("/api/users", (request, response) => {
+  response.json(users);
+});
+
+app.post("/api/users/:id/exercises", (request, response) => {});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
